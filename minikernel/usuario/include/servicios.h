a89d8a1f9ec5db8715648389d80a738b0d111099
= <=>
2018-10-21 10:03:15 +0200

    Añadiendo elementos

---
+++ minikernel/usuario/include/servicios.h
@@ -0,0 +1,44 @@
+/*
+ *  usuario/include/servicios.h
+ *
+ *  Minikernel. Versi�n 1.0
+ *
+ *  Fernando P�rez Costoya
+ *
+ */
+
+/*
+ *
+ * Fichero de cabecera que contiene los prototipos de funciones de
+ * biblioteca que proporcionan la interfaz de llamadas al sistema.
+ *
+ *      SE DEBE MODIFICAR AL INCLUIR NUEVAS LLAMADAS
+ *
+ */
+
+#ifndef SERVICIOS_H
+#define SERVICIOS_H
+
+/* Evita el uso del printf de la bilioteca est�ndar */
+#define printf escribirf
+
+/* Funcion de biblioteca */
+int escribirf(const char *formato, ...);
+
+/* Llamadas al sistema proporcionadas */
+int crear_proceso(char *prog);
+int terminar_proceso();
+int escribir(char *texto, unsigned int longi);
+
+/*
+	16 / 10 / 2018
+*/
+int obtener_id_pr();
+
+/*
+	19 / 10 / 2018
+*/
+int dormir(unsigned int s);
+
+#endif /* SERVICIOS_H */
+
